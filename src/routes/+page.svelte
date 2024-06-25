3cec3c0cf26293ae36d04dec296adf05c1de8fc6
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2024-06-25 17:08:52 +0900

    Add Mailchimp subscription form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+	import { X } from '@steeze-ui/lucide-icons';
+	import { Icon } from '@steeze-ui/svelte-icon';
 	import Chat from './Chat.svelte';
+	import SubscribeForm from './SubscribeForm.svelte';
+
+	let subscribeDialog: HTMLDialogElement;
 </script>
 
 <header class="px-4 py-4 bg-loro-green-lighter">
@@ -23,7 +28,9 @@
 
 		<div class="flex-1 flex justify-center items-center">
 			<div class="w-full text-center bg-gray-100 py-1">
-				<button>Join Waiting List</button>
+				<button class="underline" on:click={() => subscribeDialog.showModal()}
+					>Join Waiting List</button
+				>
 			</div>
 		</div>
 	</div>
@@ -47,3 +54,20 @@
 		</div>
 	</div>
 </section>
+
+<dialog
+	bind:this={subscribeDialog}
+	class="m-0 max-h-none max-w-none bg-transparent backdrop:bg-black backdrop:bg-opacity-50"
+>
+	<div class="flex h-dvh w-screen items-center justify-center px-4 sm:px-12 py-12">
+		<div class="relative w-full max-w-xl bg-white">
+			<SubscribeForm />
+
+			<form method="dialog" class="absolute right-0 top-0">
+				<button autofocus type="submit">
+					<Icon src={X} class="w-12 h-12" />
+				</button>
+			</form>
+		</div>
+	</div>
+</dialog>
---
+++ src/routes/SubscribeForm.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" />
+	<style>
+		#mc_embed_signup {
+			background: #fff;
+			clear: left;
+			font:
+				14px Helvetica,
+				Arial,
+				sans-serif;
+			width: 100%;
+		}
+		/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+			 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+	</style>
+	<script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+	<script>
+		(function ($) {
+			window.fnames = new Array();
+			window.ftypes = new Array();
+			fnames[0] = 'EMAIL';
+			ftypes[0] = 'email';
+			fnames[1] = 'FNAME';
+			ftypes[1] = 'text';
+			fnames[2] = 'LNAME';
+			ftypes[2] = 'text';
+			fnames[3] = 'ADDRESS';
+			ftypes[3] = 'address';
+			fnames[4] = 'PHONE';
+			ftypes[4] = 'phone';
+			fnames[5] = 'BIRTHDAY';
+			ftypes[5] = 'birthday';
+		})(jQuery);
+		var $mcj = jQuery.noConflict(true);
+	</script>
+</svelte:head>
+
+<div id="mc_embed_shell">
+	<div id="mc_embed_signup">
+		<form
+			action="https://gmail.us22.list-manage.com/subscribe/post?u=18126c54eb55cc0925c296377&amp;id=e02f68c436&amp;f_id=00edcfe1f0"
+			method="post"
+			id="mc-embedded-subscribe-form"
+			name="mc-embedded-subscribe-form"
+			class="validate"
+			target="_blank"
+		>
+			<div id="mc_embed_signup_scroll">
+				<h2>Subscribe</h2>
+				<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+				<div class="mc-field-group">
+					<label for="mce-EMAIL">Endereço de e-mail <span class="asterisk">*</span></label><input
+						type="email"
+						name="EMAIL"
+						class="required email"
+						id="mce-EMAIL"
+						required
+						value=""
+					/>
+				</div>
+				<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display: none;"></div>
+					<div class="response" id="mce-success-response" style="display: none;"></div>
+				</div>
+				<div aria-hidden="true" style="position: absolute; left: -5000px;">
+					<input type="text" name="b_18126c54eb55cc0925c296377_e02f68c436" tabindex="-1" value="" />
+				</div>
+				<div class="clear">
+					<input
+						type="submit"
+						name="subscribe"
+						id="mc-embedded-subscribe"
+						class="button"
+						value="Subscribe"
+					/>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
